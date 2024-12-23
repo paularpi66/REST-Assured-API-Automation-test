58a0d66610e1844e612057383614eedc98ac1058
Arpita Paul <72724463+paularpi66@users.noreply.github.com>
2024-12-23 16:38:26 +0600

    Update LimitOrder.java

--- src/test/java/LimitOrder.java
+++ src/test/java/LimitOrder.java
@@ -1,5 +1,6 @@
 //package test.java;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -12,12 +13,12 @@ import static org.hamcrest.Matchers.equalTo;
 
 
 public class LimitOrder {
-    @Test
+    @Test (priority = 1)
     public void setLimitOrder() {
-        // =========== log in =============
+        // =========== sign in =============
         Response responseLogin = given()
                 .header("Content-Type", "application/json")
-                .body("{\"identifier\": \"1900000000\",\"countryCode\": \"880\",\"identifier_type\": \"phone\",\"password\": \"0000\"}")
+                .body("{\"identifier\": \"1400000000\",\"countryCode\": \"880\",\"identifier_type\": \"phone\",\"password\": \"1111\"}")
                 .when()
                 .post("https://api-dev.techetronventures.com/api/v1/auth/login");
 
@@ -25,27 +26,58 @@ public class LimitOrder {
                 .assertThat().statusCode(200);
 
         String session = responseLogin.getBody().jsonPath().get("data.session");
-        System.out.println("++++ " + session);
+        Assert.assertNotNull(session);
+        System.out.println("Step 1++++ " + session);
 
-        // =========== ticker list =============
+        // =========== sign in otp =============
+
+        Response responsesignInOTP = given()
+                //.header() empty header
+                .body("{\"code\":\"0000\",\"session\":\""+session+"\",\"deviceInfo\":{\"country\":\"BD\",\"deviceModel\":\"M1 chip\",\"deviceName\":\"TVL Macbook-pro\",\"deviceId\":\"1f930014983c5e17d68df9c7f501cc49\",\"platform\":\"ios\"}}")
+                .post("https://api-dev.techetronventures.com/api/v1/auth/login/otp"); // 0000 = otp
+
+        responsesignInOTP.then()
+                .assertThat().statusCode(200);
+
+        String sessionOTP = responsesignInOTP.getBody().jsonPath().get("data.access");
+        System.out.println("Step 2++++ " + sessionOTP);
+
+        // =========== Get User =============
+
         given()
-                .header("STOCKX-AUTH", "<access_token>") // todo: collect access token
-                .param("userId", 711)
-                .param("clientCode", 62190)
+                //.header() header empty
                 .when()
-                .get("https://api-dev.techetronventures.com/api/v1/portfolio/trigger-list")
+                .get("https://api-dev.techetronventures.com/api/v1/auth/user?access="+sessionOTP+"&boStatus=UNKNOWN_BO_StatusType&includeMetaInfo=true&includePortfolioMeta=true&includeKycInfo=true")
                 .then()
                 .assertThat().statusCode(200);
+        System.out.println("Step 3+++++++");
+
+
+        // =========== ticker list =============
+        String Access_Token = sessionOTP;
+        Response responseTickerList = given()
+                .header("STOCKX-AUTH", Access_Token)
+                //.param("userId", 710)
+                //.param("clientCode", 62185)
+                .when()
+                .get("https://api-dev.techetronventures.com/api/v1/portfolio/trigger-list?userId=710&clientCode=62185");
+        responseTickerList.then()
+                .assertThat().statusCode(200);
+        Integer randomTickerID = Integer.valueOf(responseTickerList.getBody().jsonPath().get("data[0].tickerId"));
+        System.out.println("Step 4+++++++" + randomTickerID);
+
+
 
         // =========== set limit =============
         given()
                 .header("Content-Type", "application/json")
-                .header("STOCKX-AUTH", "<access_token>") // todo: collect access token
-                .body("{\"identifier\": \"1900000000}")
+                .header("STOCKX-AUTH", Access_Token)
+                .body("{\"userId\":\"438\",\"clientCode\":\"61257\",\"orderType\":\"1\",\"tickerId\":"+randomTickerID+",\"quantity\":1,\"limitPrice\":43,\"instructionType\":1}")
                 .when()
-                .post("https://api-dev.techetronventures.com/api/v1/auth/login")
+                .post("https://api-dev.techetronventures.com/place-order")
                 .then()
                 .assertThat().statusCode(200);
+        System.out.println("Step 5+++++++" );
 
         // =========== place order =============
         given()
